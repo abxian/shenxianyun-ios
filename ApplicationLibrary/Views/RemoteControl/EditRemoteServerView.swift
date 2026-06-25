SagerNet/sing-box-for-apple

Add remote control

ApplicationLibrary/Views/RemoteControl/EditRemoteServerView.swift
@@ -1,0 +1,184 @@
+import Library
+import SwiftUI
+
+@MainActor
+public struct EditRemoteServerView: View {
+    @EnvironmentObject private var environments: ExtensionEnvironments
+    @Environment(\.dismiss) private var dismiss
+
+    private let origin: RemoteServer?
+    private let onChanged: () async -> Void
+
+    @State private var name: String
+    @State private var url: String
+    @State private var secret: String
+
+    @State private var alert: AlertState?
+
+    public init(_ server: RemoteServer?, onChanged: @escaping () async -> Void) {
+        origin = server
+        self.onChanged = onChanged
+        _name = State(initialValue: server?.name ?? "")
+        _url = State(initialValue: server?.url ?? "")
+        _secret = State(initialValue: server?.secret ?? "")
+    }
+
+    private var title: String {
+        origin == nil ? String(localized: "New Server") : String(localized: "Edit Server")
+    }
+
+    public var body: some View {
+        #if os(macOS)
+            macOSBody
+        #else
+            iOSBody
+        #endif
+    }
+
+    private var formContent: some View {
+        FormView {
+            Section("Server") {
+                FormItem(String(localized: "Name")) {
+                    TextField("Name", text: $name, prompt: Text("Required"))
+                        .multilineTextAlignment(.trailing)
+                }
+                FormItem(String(localized: "URL")) {
+                    TextField("URL", text: $url, prompt: Text("Required"))
+                        .multilineTextAlignment(.trailing)
+                    #if os(iOS) || os(tvOS)
+                        .textContentType(.URL)
+                        .keyboardType(.URL)
+                        .textInputAutocapitalization(.never)
+                        .autocorrectionDisabled(true)
+                    #endif
+                }
+                FormItem(String(localized: "Secret")) {
+                    SecureField("Secret", text: $secret, prompt: Text("Optional"))
+                        .multilineTextAlignment(.trailing)
+                    #if os(iOS)
+                        .textContentType(.init(rawValue: ""))
+                    #endif
+                }
+            }
+            #if !os(macOS)
+                Section {
+                    FormButton {
+                        Task {
+                            await save()
+                        }
+                    } label: {
+                        Label("Save", systemImage: "doc.fill")
+                    }
+                }
+            #endif
+            #if os(tvOS)
+                if origin != nil {
+                    Section {
+                        FormButton(role: .destructive) {
+                            Task {
+                                await deleteServer()
+                            }
+                        } label: {
+                            Label("Delete", systemImage: "trash.fill")
+                                .foregroundColor(.red)
+                        }
+                    }
+                }
+            #endif
+        }
+    }
+
+    #if os(macOS)
+        private var macOSBody: some View {
+            VStack(alignment: .leading, spacing: 0) {
+                Text(title)
+                    .font(.headline)
+                    .padding(.horizontal, 20)
+                    .padding(.top, 20)
+                    .padding(.bottom, 12)
+
+                formContent
+            }
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                }
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Save") {
+                        Task {
+                            await save()
+                        }
+                    }
+                }
+            }
+            .alert($alert)
+        }
+    #else
+        private var iOSBody: some View {
+            formContent
+                .navigationTitle(title)
+                .alert($alert)
+        }
+    #endif
+
+    private func validate() -> String? {
+        if name.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            alert = AlertState(errorMessage: String(localized: "Missing server name"))
+            return nil
+        }
+        do {
+            return try RemoteServer.validateURL(url)
+        } catch {
+            alert = AlertState(action: "parse server URL", error: error)
+            return nil
+        }
+    }
+
+    private func applyChanges(to server: RemoteServer, url: String) {
+        server.name = name.trimmingCharacters(in: .whitespacesAndNewlines)
+        server.url = url
+        server.secret = secret
+    }
+
+    private func save() async {
+        guard let validatedURL = validate() else {
+            return
+        }
+        do {
+            if let origin {
+                applyChanges(to: origin, url: validatedURL)
+                try await RemoteServerManager.update(origin)
+            } else {
+                let server = RemoteServer(name: "")
+                applyChanges(to: server, url: validatedURL)
+                try await RemoteServerManager.create(server)
+            }
+        } catch {
+            alert = AlertState(action: "save server", error: error)
+            return
+        }
+        await onChanged()
+        dismiss()
+    }
+
+    #if os(tvOS)
+        private func deleteServer() async {
+            guard let origin else {
+                return
+            }
+            do {
+                if environments.remoteServer?.id == origin.id {
+                    environments.exitRemoteControl()
+                }
+                try await RemoteServerManager.delete(origin)
+            } catch {
+                alert = AlertState(action: "delete server", error: error)
+                return
+            }
+            await onChanged()
+            dismiss()
+        }
+    #endif
+}

ApplicationLibrary/Views/RemoteControl/RemoteControlMenuItems.swift
@@ -1,0 +1,59 @@
+#if os(iOS) || os(macOS)
+    import Library
+    import SwiftUI
+
+    public struct RemoteControlMenuItems: View {
+        @EnvironmentObject private var environments: ExtensionEnvironments
+        private let servers: [RemoteServer]
+
+        public init(servers: [RemoteServer]) {
+            self.servers = servers
+        }
+
+        public var body: some View {
+            if !servers.isEmpty {
+                Section("Remote Control") {
+                    // iOS 15 menus do not render section header text
+                    if #unavailable(iOS 16.0) {
+                        Text("Remote Control")
+                    }
+                    localDeviceButton
+                    ForEach(servers) { server in
+                        serverButton(server)
+                    }
+                    Button {
+                        NotificationCenter.default.post(name: .navigateToSettingsPage, object: SettingsPage.remoteControl)
+                    } label: {
+                        Label("Manage Servers...", systemImage: "slider.horizontal.3")
+                    }
+                }
+            }
+        }
+
+        private var localDeviceButton: some View {
+            Button {
+                environments.exitRemoteControl()
+            } label: {
+                if environments.remoteServer == nil {
+                    Label("Local Device", systemImage: "checkmark")
+                } else {
+                    Text("Local Device")
+                }
+            }
+        }
+
+        private func serverButton(_ server: RemoteServer) -> some View {
+            let isActive = environments.remoteServer?.id == server.id
+            return Button {
+                guard !isActive else { return }
+                environments.enterRemoteControl(server)
+            } label: {
+                if isActive {
+                    Label(server.name, systemImage: "checkmark")
+                } else {
+                    Text(server.name)
+                }
+            }
+        }
+    }
+#endif

SFI/MainView.swift
@@ -74,7 +74,9 @@ struct MainView: View {
 
     @ViewBuilder
     private var accessoryInset: some View {
-        if let profile = environments.extensionProfile, !environments.extensionProfileLoading, !environments.emptyProfiles {
+        if environments.remoteServer != nil {
+            remoteStatusBarPill
+        } else if let profile = environments.extensionProfile, !environments.extensionProfileLoading, !environments.emptyProfiles {
             AccessoryInset(profile: profile) {
                 statusBarPill
             } fab: {
@@ -102,6 +104,66 @@ struct MainView: View {
             .padding(.horizontal, 20)
             .padding(.top, 8)
             .padding(.bottom, 12)
+    }
+
+    private var remoteStatusBarPill: some View {
+        remoteAccessoryContent
+            .frame(maxWidth: .infinity)
+            .frame(height: 44)
+            .modifier(AccessoryPillBackgroundModifier(cornerRadius: 22))
+            .contentShape(RoundedRectangle(cornerRadius: 22, style: .continuous))
+            .padding(.horizontal, 20)
+            .padding(.top, 8)
+            .padding(.bottom, 12)
+    }
+
+    private var remoteAccessoryContent: some View {
+        HStack(spacing: 12) {
+            RemoteStatusText(
+                commandClient: environments.commandClient,
+                serverName: environments.remoteServer?.name ?? ""
+            )
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+            NavigationButtonsView(
+                showGroupsButton: buttonState.showGroupsButton,
+                showConnectionsButton: buttonState.showConnectionsButton,
+                groupsCount: buttonState.groupsCount,
+                connectionsCount: buttonState.connectionsCount,
+                onGroupsTap: { showGroups = true },
+                onConnectionsTap: { showConnections = true }
+            )
+            Divider()
+            RemoteUptimeText(commandClient: environments.commandClient)
+            Button {
+                environments.exitRemoteControl()
+            } label: {
+                Label("Disconnect", systemImage: "antenna.radiowaves.left.and.right.slash")
+                    .labelStyle(.iconOnly)
+            }
+        }
+        .padding(.horizontal)
+        .tint(.primary)
+        .buttonStyle(BarItemButtonStyle())
+    }
+
+    private struct RemoteStatusText: View {
+        @ObservedObject var commandClient: CommandClient
+        let serverName: String
+
+        var body: some View {
+            statusText
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                .lineLimit(1)
+        }
+
+        private var statusText: Text {
+            if commandClient.isConnected {
+                return Text(serverName)
+            } else {
+                return Text("Connecting...")
+            }
+        }
     }
 
     private struct AccessoryPillBackgroundModifier: ViewModifier {
@@ -162,6 +224,15 @@ struct MainView: View {
                 .onReceive(environments.commandClient.$hasAnyConnection) { _ in
                     Task { @MainActor in updateButtonVisibility() }
                 }
+                .onReceive(environments.commandClient.$isConnected) { _ in
+                    Task { @MainActor in updateButtonVisibility() }
+                }
+                .onReceive(environments.commandClient.statusPublisher) { _ in
+                    Task { @MainActor in updateButtonVisibility() }
+                }
+                .onReceive(environments.$remoteServer) { _ in
+                    Task { @MainActor in updateButtonVisibility() }
+                }
                 .onReceive(NotificationCenter.default.publisher(for: .NEVPNStatusDidChange)) { _ in
                     Task { @MainActor in updateButtonVisibility() }
                 }
@@ -200,6 +271,10 @@ struct MainView: View {
                 selection = .tools
             }
         }
+        .onReceive(NotificationCenter.default.publisher(for: .navigateToSettingsPage)) { notification in
+            guard notification.object is SettingsPage else { return }
+            selection = .settings
+        }
         .environment(\.selection, $selection)
         .environment(\.importProfile, $importProfile)
         .environment(\.importRemoteProfile, $importRemoteProfile)
@@ -210,10 +285,18 @@ struct MainView: View {
     }
 
     private func updateButtonVisibility() {
-        buttonState.update(
-            profile: environments.extensionProfile,
-            commandClient: environments.commandClient
-        )
+        var newState = buttonState
+        if environments.remoteServer != nil {
+            newState.update(remoteClient: environments.commandClient)
+        } else {
+            newState.update(
+                profile: environments.extensionProfile,
+                commandClient: environments.commandClient
+            )
+        }
+        if newState != buttonState {
+            buttonState = newState
+        }
     }
 
     private struct AccessoryInset<StatusBar: View, FAB: View>: View {

abxian/shenxianyun-ios

ApplicationLibrary/Views/RemoteControl/EditRemoteServerView.swift
@@ -1,0 +1,181 @@
+import Library
+import SwiftUI
+
+@MainActor
+public struct EditRemoteServerView: View {
+    @EnvironmentObject private var environments: ExtensionEnvironments
+    @Environment(\.dismiss) private var dismiss
+
+    private let origin: RemoteServer?
+    private let onChanged: () async -> Void
+
+    @State private var name: String
+    @State private var url: String
+    @State private var secret: String
+
+    @State private var alert: AlertState?
+
+    public init(_ server: RemoteServer?, onChanged: @escaping () async -> Void) {
+        origin = server
+        self.onChanged = onChanged
+        _name = State(initialValue: server?.name ?? "")
+        _url = State(initialValue: server?.url ?? "")
+        _secret = State(initialValue: server?.secret ?? "")
+    }
+
+    private var title: String {
+        origin == nil ? String(localized: "New Server") : String(localized: "Edit Server")
+    }
+
+    public var body: some View {
+        #if os(macOS)
+            macOSBody
+        #else
+            iOSBody
+        #endif
+    }
+
+    private var formContent: some View {
+        FormView {
+            Section("Server") {
+                FormItem(String(localized: "Name")) {
+                    TextField("Name", text: $name, prompt: Text("Optional"))
+                        .multilineTextAlignment(.trailing)
+                }
+                FormItem(String(localized: "URL")) {
+                    TextField("URL", text: $url, prompt: Text("Required"))
+                        .multilineTextAlignment(.trailing)
+                    #if os(iOS) || os(tvOS)
+                        .textContentType(.URL)
+                        .keyboardType(.URL)
+                        .textInputAutocapitalization(.never)
+                        .autocorrectionDisabled(true)
+                    #endif
+                }
+                FormItem(String(localized: "Secret")) {
+                    SecureField("Secret", text: $secret, prompt: Text("Optional"))
+                        .multilineTextAlignment(.trailing)
+                    #if os(iOS)
+                        .textContentType(.init(rawValue: ""))
+                    #endif
+                }
+            }
+            #if !os(macOS)
+                Section {
+                    FormButton {
+                        Task {
+                            await save()
+                        }
+                    } label: {
+                        Label("Save", systemImage: "doc.fill")
+                    }
+                }
+            #endif
+            #if os(tvOS)
+                if origin != nil {
+                    Section {
+                        FormButton(role: .destructive) {
+                            Task {
+                                await deleteServer()
+                            }
+                        } label: {
+                            Label("Delete", systemImage: "trash.fill")
+                                .foregroundColor(.red)
+                        }
+                    }
+                }
+            #endif
+        }
+    }
+
+    #if os(macOS)
+        private var macOSBody: some View {
+            VStack(alignment: .leading, spacing: 0) {
+                Text(title)
+                    .font(.headline)
+                    .padding(.horizontal, 20)
+                    .padding(.top, 20)
+                    .padding(.bottom, 12)
+
+                formContent
+            }
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                }
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Save") {
+                        Task {
+                            await save()
+                        }
+                    }
+                }
+            }
+            .alert($alert)
+        }
+    #else
+        private var iOSBody: some View {
+            formContent
+                .navigationTitle(title)
+                .alert($alert)
+        }
+    #endif
+
+    private func validate() -> String? {
+        do {
+            return try RemoteServer.validateURL(url)
+        } catch {
+            alert = AlertState(action: "parse server URL", error: error)
+            return nil
+        }
+    }
+
+    private func applyChanges(to server: RemoteServer, url: String) {
+        let trimmedName = name.trimmingCharacters(in: .whitespacesAndNewlines)
+        server.name = trimmedName.isEmpty ? nil : trimmedName
+        server.url = url
+        server.secret = secret
+    }
+
+    private func save() async {
+        guard let validatedURL = validate() else {
+            return
+        }
+        do {
+            if let origin {
+                applyChanges(to: origin, url: validatedURL)
+                try await RemoteServerManager.update(origin)
+            } else {
+                let server = RemoteServer()
+                applyChanges(to: server, url: validatedURL)
+                try await RemoteServerManager.create(server)
+            }
+        } catch {
+            alert = AlertState(action: "save server", error: error)
+            return
+        }
+        await onChanged()
+        dismiss()
+    }
+
+    #if os(tvOS)
+        private func deleteServer() async {
+            guard let origin else {
+                return
+            }
+            do {
+                if environments.remoteServer?.id == origin.id {
+                    environments.exitRemoteControl()
+                }
+                try await RemoteServerManager.delete(origin)
+            } catch {
+                alert = AlertState(action: "delete server", error: error)
+                return
+            }
+            await onChanged()
+            dismiss()
+        }
+    #endif
+}

ApplicationLibrary/Views/RemoteControl/RemoteControlMenuItems.swift
@@ -1,0 +1,59 @@
+#if os(iOS) || os(macOS)
+    import Library
+    import SwiftUI
+
+    public struct RemoteControlMenuItems: View {
+        @EnvironmentObject private var environments: ExtensionEnvironments
+        private let servers: [RemoteServer]
+
+        public init(servers: [RemoteServer]) {
+            self.servers = servers
+        }
+
+        public var body: some View {
+            if !servers.isEmpty {
+                Section("Remote Control") {
+                    // iOS 15 menus do not render section header text
+                    if #unavailable(iOS 16.0) {
+                        Text("Remote Control")
+                    }
+                    localDeviceButton
+                    ForEach(servers) { server in
+                        serverButton(server)
+                    }
+                    Button {
+                        NotificationCenter.default.post(name: .navigateToSettingsPage, object: SettingsPage.remoteControl)
+                    } label: {
+                        Label("Manage Servers...", systemImage: "slider.horizontal.3")
+                    }
+                }
+            }
+        }
+
+        private var localDeviceButton: some View {
+            Button {
+                environments.exitRemoteControl()
+            } label: {
+                if environments.remoteServer == nil {
+                    Label("Local Device", systemImage: "checkmark")
+                } else {
+                    Text("Local Device")
+                }
+            }
+        }
+
+        private func serverButton(_ server: RemoteServer) -> some View {
+            let isActive = environments.remoteServer?.id == server.id
+            return Button {
+                guard !isActive else { return }
+                environments.enterRemoteControl(server)
+            } label: {
+                if isActive {
+                    Label(server.displayName, systemImage: "checkmark")
+                } else {
+                    Text(server.displayName)
+                }
+            }
+        }
+    }
+#endif

SFI/MainView.swift
@@ -74,7 +74,9 @@ struct MainView: View {
 
     @ViewBuilder
     private var accessoryInset: some View {
-        if let profile = environments.extensionProfile, !environments.extensionProfileLoading, !environments.emptyProfiles {
+        if environments.remoteServer != nil {
+            remoteStatusBarPill
+        } else if let profile = environments.extensionProfile, !environments.extensionProfileLoading, !environments.emptyProfiles {
             AccessoryInset(profile: profile) {
                 statusBarPill
             } fab: {
@@ -102,6 +104,66 @@ struct MainView: View {
             .padding(.horizontal, 20)
             .padding(.top, 8)
             .padding(.bottom, 12)
+    }
+
+    private var remoteStatusBarPill: some View {
+        remoteAccessoryContent
+            .frame(maxWidth: .infinity)
+            .frame(height: 44)
+            .modifier(AccessoryPillBackgroundModifier(cornerRadius: 22))
+            .contentShape(RoundedRectangle(cornerRadius: 22, style: .continuous))
+            .padding(.horizontal, 20)
+            .padding(.top, 8)
+            .padding(.bottom, 12)
+    }
+
+    private var remoteAccessoryContent: some View {
+        HStack(spacing: 12) {
+            RemoteStatusText(
+                commandClient: environments.commandClient,
+                serverName: environments.remoteServer?.displayName ?? ""
+            )
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+            NavigationButtonsView(
+                showGroupsButton: buttonState.showGroupsButton,
+                showConnectionsButton: buttonState.showConnectionsButton,
+                groupsCount: buttonState.groupsCount,
+                connectionsCount: buttonState.connectionsCount,
+                onGroupsTap: { showGroups = true },
+                onConnectionsTap: { showConnections = true }
+            )
+            Divider()
+            RemoteUptimeText(commandClient: environments.commandClient)
+            Button {
+                environments.exitRemoteControl()
+            } label: {
+                Label("Disconnect", systemImage: "antenna.radiowaves.left.and.right.slash")
+                    .labelStyle(.iconOnly)
+            }
+        }
+        .padding(.horizontal)
+        .tint(.primary)
+        .buttonStyle(BarItemButtonStyle())
+    }
+
+    private struct RemoteStatusText: View {
+        @ObservedObject var commandClient: CommandClient
+        let serverName: String
+
+        var body: some View {
+            statusText
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                .lineLimit(1)
+        }
+
+        private var statusText: Text {
+            if commandClient.isConnected {
+                return Text(serverName)
+            } else {
+                return Text("Connecting...")
+            }
+        }
     }
 
     private struct AccessoryPillBackgroundModifier: ViewModifier {
@@ -162,6 +224,15 @@ struct MainView: View {
                 .onReceive(environments.commandClient.$hasAnyConnection) { _ in
                     Task { @MainActor in updateButtonVisibility() }
                 }
+                .onReceive(environments.commandClient.$isConnected) { _ in
+                    Task { @MainActor in updateButtonVisibility() }
+                }
+                .onReceive(environments.commandClient.statusPublisher) { _ in
+                    Task { @MainActor in updateButtonVisibility() }
+                }
+                .onReceive(environments.$remoteServer) { _ in
+                    Task { @MainActor in updateButtonVisibility() }
+                }
                 .onReceive(NotificationCenter.default.publisher(for: .NEVPNStatusDidChange)) { _ in
                     Task { @MainActor in updateButtonVisibility() }
                 }
@@ -200,6 +271,10 @@ struct MainView: View {
                 selection = .tools
             }
         }
+        .onReceive(NotificationCenter.default.publisher(for: .navigateToSettingsPage)) { notification in
+            guard notification.object is SettingsPage else { return }
+            selection = .settings
+        }
         .environment(\.selection, $selection)
         .environment(\.importProfile, $importProfile)
         .environment(\.importRemoteProfile, $importRemoteProfile)
@@ -210,10 +285,18 @@ struct MainView: View {
     }
 
     private func updateButtonVisibility() {
-        buttonState.update(
-            profile: environments.extensionProfile,
-            commandClient: environments.commandClient
-        )
+        var newState = buttonState
+        if environments.remoteServer != nil {
+            newState.update(remoteClient: environments.commandClient)
+        } else {
+            newState.update(
+                profile: environments.extensionProfile,
+                commandClient: environments.commandClient
+            )
+        }
+        if newState != buttonState {
+            buttonState = newState
+        }
     }
 
     private struct AccessoryInset<StatusBar: View, FAB: View>: View {